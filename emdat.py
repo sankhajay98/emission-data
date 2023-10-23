8b1856148b72ae90c8f99126dccbb1f3cbc49508
sankhajay98 <100501110+sankhajay98@users.noreply.github.com>
2023-10-23 12:03:48 +0530

    Changed to CSV

---
+++ emdat.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from tqdm import tqdm
+
+# Define the file paths for your input CSV files
+file1_path = 'input/2018-01.csv'
+file2_path = 'input/2019-01.csv'
+
+# Load both CSV files into pandas dataframes
+df1 = pd.read_csv(file1_path)
+df2 = pd.read_csv(file2_path)
+
+# Merge dataframes based on the 'VehRegNo' column
+merged_df = df1.merge(df2, on='VehRegNo')
+
+# Create an empty list to store distance values
+distances = []
+
+# Create a tqdm progress bar
+with tqdm(total=len(merged_df), desc="Processing") as pbar:
+    for _, row in merged_df.iterrows():
+        try:
+            # Convert mileage values to numeric (float)
+            mileage1 = float(row['VehCurrentMileage_x'])
+            mileage2 = float(row['VehCurrentMileage_y'])
+            # Calculate the distance traveled
+            distance = abs(mileage1 - mileage2)
+            distances.append(distance)
+        except ValueError:
+            # Handle non-numeric values or conversion errors
+            distances.append(None)  # You can choose how to handle these cases
+        pbar.update(1)
+
+# Add the 'DistanceTraveled' column to the merged dataframe
+merged_df['DistanceTraveled'] = distances
+
+# Create a new dataframe with just the 'VehRegNo' and 'DistanceTraveled' columns
+result_df = merged_df[['VehRegNo', 'DistanceTraveled']]
+
+# Save the result to a new CSV file
+result_file = 'output/vehicle_distance.csv'
+result_df.to_csv(result_file, index=False)
+
+print(f"Results saved to {result_file}")
